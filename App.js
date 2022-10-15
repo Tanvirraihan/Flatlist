b32ff2dfadf90e0c0e66054328cced60a2d63e9b
M. R. Tanvir <tanvirraihan71@gmail.com>
2022-10-16 04:25:03 +0600

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+ const languages = [
+  { name: 'Founders of modern Bengali poetry' , key: '1' },
+  { name: 'Rabindranath Tagore' , key: '2' },
+  { name: 'Jibanananda Das' , key: '3' },
+  { name: 'Jasimuddin' , key: '4' },
+  { name: 'Kaykobad' , key: '5' },
+  { name: 'Michael Madhusudan Dutta' , key: '6' },
+  { name: 'Kazi Nazrul Islam' , key: '7' },
 
+]
+return (
+  <View style={styles.container}>
+   {languages.map((lang) => (
+     <View key={lang.key}>
+      <Text style={styles.item}>{lang.name}</Text>
+     </View>
+   ))}
+  </View>
+ );
+}
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+container: {
+  flex: 1,
+  backgroundColor: '#fff',
+  paddingTop: 40,
+  paddingHorizontal: 20
+},
+item: {
+  marginTop: 20,
+  padding: 30,
+  backgroundColor: '#55b5db',
+  fontSize: 24
+}
 });
